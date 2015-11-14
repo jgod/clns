5e458f66d48c94368fe8ecbeabbe8973eee64173
Justin Godesky <justingodesky@gmail.com>
2015-11-14 06:50:52 -0500

    Initial commit: - Basic functionality - Tests - Makefile - Readme

---
+++ clns.h
@@ -0,0 +1,108 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2015 Justin Godesky
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#ifndef clns_h
+#define clns_h
+
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+namespace jgod { namespace clns {
+  template <typename T>
+  struct identity {typedef T type;}; // Used to coerce template arguments
+
+#pragma mark - Searching
+  /** Returns whether a vector of elements includes a certain element. */
+  template <typename T>
+  bool includes(const std::vector<T> &v, const typename identity<T>::type el) {
+    return std::find_if(std::begin(v), std::end(v), [&](const T &i) {
+      return i == el;
+    }) != std::end(v);
+  }
+
+  /** Sorts and returns whether a vector of elements contain all of another vector. */
+  template <typename T>
+  bool includes(std::vector<T> &a, std::vector<T> &b) {
+    std::sort(std::begin(a), std::end(a));
+    std::sort(std::begin(b), std::end(b));
+    return std::includes(std::begin(a), std::end(a),
+                         std::begin(b), std::end(b));
+  }
+
+#pragma mark - Modifying
+  template <typename T>
+  void compact(std::vector<T> &v) {
+    v.erase(std::remove_if(v.begin(), v.end(), [](const T &t){return !t;}), v.end());
+  }
+  void compact(std::vector<std::string> &v) {
+    v.erase(std::remove(v.begin(), v.end(), ""), v.end());
+  }
+
+#pragma mark - Sets
+  template <typename T>
+  /** Returns a vector of all elements between two (unsorted) vectors of elements. */
+  const std::vector<T> getUnion(std::vector<T> &a, std::vector<T> &b) {
+    std::vector<T> un(a.size() + b.size());
+    std::sort(std::begin(a), std::end(a));
+    std::sort(std::begin(b), std::end(b));
+
+    typename std::vector<T>::iterator it;
+    it = std::set_union(std::begin(a), std::end(a),
+                        std::begin(b), std::end(b), std::begin(un));
+    un.resize(it - un.begin());
+    return un;
+  }
+
+  template <typename T>
+  /** Returns a vector of matched elements between two (unsorted) vectors of elements. */
+  const std::vector<T> getIntersection(std::vector<T> &a, std::vector<T> &b) {
+    std::vector<T> xion(a.size() + b.size());
+    std::sort(std::begin(a), std::end(a));
+    std::sort(std::begin(b), std::end(b));
+
+    typename std::vector<T>::iterator it;
+    it = std::set_intersection(std::begin(a), std::end(a),
+                               std::begin(b), std::end(b),
+                               xion.begin());
+    xion.resize(it - xion.begin());
+    return xion;
+  }
+
+  template <typename T>
+  /** Returns a vector of diffed elements between two (unsorted) vectors of elements. */
+  const std::vector<T> getDifference(std::vector<T> &a, std::vector<T> &b) {
+    std::vector<T> diff(a.size() + b.size());
+    std::sort(std::begin(a), std::end(a));
+    std::sort(std::begin(b), std::end(b));
+
+    typename std::vector<T>::iterator it;
+    it = std::set_symmetric_difference(std::begin(a), std::end(a),
+                                       std::begin(b), std::end(b),
+                                       diff.begin());
+    diff.resize(it - diff.begin());
+    return diff;
+  }
+}}
+#endif /* clns_h */
---
+++ tests/main.cpp
@@ -0,0 +1,101 @@
+//
+//  main.cpp
+//  clns
+//
+//  Created by Justin Godesky on 11/14/15.
+//
+//
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include "../clns.h"
+using namespace jgod;
+
+TEST_CASE("searching") {
+  SECTION("strings") {
+    SECTION("includes") {
+      SECTION("value") {
+        const auto C = std::vector<std::string>{"a", "b", "c"};
+        REQUIRE(clns::includes(C, "b"));
+      }
+
+      SECTION("another collection") {
+        auto a = std::vector<std::string>{"a", "b", "c"};
+        auto b = std::vector<std::string>{"a", "b"};
+        REQUIRE(clns::includes(a, b));
+      }
+    }
+  }
+
+  SECTION("numbers") {
+    SECTION("includes") {
+      SECTION("value") {
+        const auto C = std::vector<int>{1, 2, 3};
+        REQUIRE(clns::includes(C, 2));
+      }
+
+      SECTION("another collection") {
+        auto a = std::vector<int>{1, 2, 3};
+        auto b = std::vector<int>{1, 2};
+        REQUIRE(clns::includes(a, b));
+      }
+    }
+  }
+}
+
+TEST_CASE("modifying") {
+  SECTION("compact") {
+    SECTION("string") {
+      auto src = std::vector<std::string>{"", "thing", "", ""};
+      auto target = std::vector<std::string>{"thing"};
+      clns::compact(src);
+      REQUIRE(src == target);
+    }
+
+    SECTION("numbers") {
+      auto src = std::vector<int>{0, 2, 0, 1};
+      auto target = std::vector<int>{2, 1};
+      clns::compact(src);
+      REQUIRE(src == target);
+    }
+  }
+}
+
+TEST_CASE("sets") {
+
+  SECTION("strings") {
+    auto a = std::vector<std::string>{"dog", "bird", "cat"};
+    auto b = std::vector<std::string>{"apple", "dog", "broom", "car"};
+
+    SECTION("get union") {
+      const auto UNIQUE = std::vector<std::string>{"apple", "bird", "broom", "car", "cat", "dog"};
+      REQUIRE(clns::getUnion(a, b) == UNIQUE);
+    }
+    SECTION("get intersection") {
+      const auto XION = std::vector<std::string>{"dog"};
+      REQUIRE(clns::getIntersection(a, b) == XION);
+    }
+    SECTION("get difference") {
+      const auto DIFF = std::vector<std::string>{"apple", "bird", "broom", "car", "cat"};
+      REQUIRE(clns::getDifference(a, b) == DIFF);
+    }
+  }
+
+  SECTION("numbers") {
+    auto a = std::vector<int>{3, 1, 2};
+    auto b = std::vector<int>{5, 1, 6, 4, 3};
+
+    SECTION("get union") {
+      const auto UNIQUE = std::vector<int>{1, 2, 3, 4, 5, 6};
+      REQUIRE(clns::getUnion(a, b) == UNIQUE);
+    }
+    SECTION("get intersection") {
+      const auto XION = std::vector<int>{1, 3};
+      REQUIRE(clns::getIntersection(a, b) == XION);
+    }
+    SECTION("get difference") {
+      const auto DIFF = std::vector<int>{2, 4, 5, 6};
+      REQUIRE(clns::getDifference(a, b) == DIFF);
+    }
+  }
+}
